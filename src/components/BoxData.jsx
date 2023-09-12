ae4720c0f0839b65e1707ee6d8fea8dcdd087875
ValdsonSilva <valdsonmacedo15@gmail.com>
2023-09-12 13:36:31 -0300

    Organizando layout e subindo alguns assets

--- src/components/BoxData.jsx
+++ src/components/BoxData.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import "./BoxData.style.css"
 
-function BoxData(props) {
+function BoxData() {
+
+  function consumo_api() {
+
+     console.info("Bora estruturar o layout dos dados!!!")
+
+    //  useQuery() {
+
+    //  }
+  }
+
   return (
+
     <div className='Boxdata'>
-        {props.children}
+        {/* {props.children} */}
+        {consumo_api()}
+        <p>DATA</p>
+        <p>LOCALIDADE</p>
+        <p>Aaaaa</p>
+        <p>Baaa</p>
+        <p>Caaa</p>
+        <p>Daaa</p>
+        <p>Faaa</p>
+        <p>10</p>
+        <p>11</p>
+        <p>12</p>
+        <p>13</p>
+        <p>14</p>
+        
     </div>
   )
 }
