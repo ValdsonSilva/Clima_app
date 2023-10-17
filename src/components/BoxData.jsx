ba8bddf39d68546b64a1ff1169677a502d222cf4
ValdsonSilva <valdsonmacedo15@gmail.com>
2023-10-17 11:20:11 -0300

    Verificação do estado da response da API implementada

--- src/components/BoxData.jsx
+++ src/components/BoxData.jsx
@@ -3,13 +3,13 @@ import "./BoxData.style.css"
 import {useQuery} from 'react-query'
 import axios from 'axios';
 import { data_limpa } from '../logic_data/Dia_semana'; 
-import sensacao from "../assets/sol_nuvem.svg"
 import seta_cima from "../assets/seta_cima.svg"
 import seta_baxio from "../assets/seta_baixo.svg"
 import sol_nuvem from "../assets/sol_nuvem.svg"
 import humidade from "../assets/humidade.svg"
 import pressao from "../assets/medidor_pressão.svg"
 import vento from "../assets/vento.svg"
+import noite_vento from "../assets/noite_vento.svg"
 
 const key = "923c6f0edf17c6425d91e6f22443e3cd"
 
@@ -17,126 +17,162 @@ const minha_url = `http://api.openweathermap.org/data/2.5/forecast?id=524901&app
 
 function BoxData() {
 
-  // const [data, setData] = useState([])
-  
-  const data = "Valdson"
+  const [dados] = useState([])
 
-  // useEffect(() => {
-  //     fetch(minha_url)
-  //     .then((response) => response.json())
-  //     .then((dado) => {
-  //       // Responsável por ativar novas requisições
-  //       // com base na mudança de estado
-  //       setData(dado)
-  //     })
-  //     .catch((erro) => {console.log(`Algo deu errado ai ${erro}!`)})
-  // },
-  //   [] // O array vazia fala para o useEffect fazer apenas 1 requizição ao renderizar o componente
-  // )
+  const {data, isFetching, isError} = useQuery(['dados', dados], async () => {
+        const response = await axios.get(minha_url)
+        return response.data
+  },{
+    // Garante que os dados sejam atualizados ao retornar para tela
+    refetchOnWindowFocus:true, 
+    // Tempo que o código vai esperar para fazer uma nova requisição
+    staleTime: 1000 * 6,
+  })
 
   console.log(data)
 
-  const constante_formula = 237.15 // deslocamento entre as escalas
+  var Temperatura_celsius, Temperatura_maxima, Temperatura_minima, Sensacao_termica;
 
-  // var Temperatura_celsius = ((data.list[0].main.temp) - constante_formula)
+  //verificando se data != Null
+  if (!data){
+     console.log("Erro no retorna da API")
 
-  // var Temperatura_maxima = ((data.list[0].main.temp_max) - constante_formula)
+  }else {
+    const constante_formula = 237.15 // deslocamento entre as escalas
+    const main = data.list?.[0]?.main; // list -> [0] -> main
 
-  // var Temperatura_minima = ((data.list[0].main.temp_min) - constante_formula)
+    Temperatura_celsius = ((main.temp) - constante_formula).toFixed(0)
+    Temperatura_maxima = ((main.temp_max) - constante_formula).toFixed(0)
+    Temperatura_minima = ((main.temp_min) - constante_formula).toFixed(0)
+    Sensacao_termica =  ((main.feels_like) - constante_formula).toFixed(0)
+  }
 
-  return (
+  // se estiver carregando
+  if (isFetching){
 
-    <div className='Boxdata'>
-        {/* Nome d.sema, data Mês ano, | hora */}
-        <p className='data_app'>{data_limpa}</p>
-        {/* País e cidade */}
-        <p>
-          {/* {data.city.name} */}
-          {data}
-        </p>
-        {/* situação climática */}
-        
-        <div className='container_sol_nuvem_padrao'>
-            <figure>
-                <img src={sol_nuvem} alt="Sol na nuvem" className='sol_nuvem'/>
-            </figure>
-            <p>
-              {/* {data.list[0].weather[0].main} */}
-              {data}
-            </p>
-        </div>
+    return <h1>Carregando dados...</h1>
 
+  // se deu erro
+  }else if (isError){
 
-        {/* Temp.máxima */}
-        <div className='temp'>
-            <p>35°C</p>
-            <figure className='container_seta'>
-                {/* imagem */}
-                <img src={seta_cima} alt="Seta para cima" className='imagem_seta' />
-            </figure>
-        </div>
-    
-        {/* Temperatura fixa */}
-        <p className='temp_fixa'>33°C</p>
+    return <h1>Erro na consulta!</h1>
 
-        {/* Temp. mínima */}
-        <div className='temp'>
-            <p>30°C</p>
-            <figure className='container_seta'>
-                {/* imagem */}
-                <img src={seta_baxio} alt="Seta para baixo" className='imagem_seta' />
-            </figure>
-        </div>       
+  // se deu certo
+  }else {
 
-        
-        {/* humidade */}
-        <div className='container_humidade_padrao'>
-          <figure>
-              <img src={humidade} alt="imgame/logo de humidade" className='humidade'/>
-              <p>49%</p>
-              <p className='nome_pequeno'>Humidade</p>
-          </figure>
-        </div>
-        
-        {/* pressão */}
-        <div className='container_pressao_padrao'>
-          <figure>
-              <img src={pressao} alt="imgame/logo de aparelho de medir pressão" className='pressao'/>
-              <p>1,007mBar</p>
-              <p className='nome_pequeno'>Pressão</p>
-          </figure>
-        </div>
+    return (
 
-        {/* vento */}
-        <div className='container_vento_padrao'>
-            <figure>
-                <img src={vento} alt="imagem de corrente de vento" className='vento'/>
-                <p>23km/h</p>
-                <p className='nome_pequeno'>Vento</p>
-            </figure>
-        </div>
-        
-        {/* latitude */}
-        <div className='container_vento_padrao'>
-            <p>23km/h</p>
-            <p className='nome_pequeno'>Latitude</p>
-        </div>
-
-        {/* logintude */}
-        <div className='container_vento_padrao'>
-          <p>13</p>
-          <p className='nome_pequeno'>Longitude</p>
-        </div>
-    
-        {/* sensação térmica */}
-        <div className='container_vento_padrao'>
-          <p>14</p>
-          <p className='nome_pequeno'>Sensação Térmica</p>
-        </div>
+        <div className='Boxdata'>
+            {/* Nome d.sema, data Mês ano, | hora */}
+            <p className='data_app'>{data_limpa}</p>
+            {/* País e cidade */}
+            {data && (
+              <p>{data.city.name}</p>
+              // {/* {pais} */}
+            )}
       
+            {/* situação climática */}
+            
+            <div className='container_sol_nuvem_padrao'>
+                <figure>
+                    {/* Esse código abaixo será usado no fim */}
+                    <img 
+                    src={(data.list[0].weather[0].main) === 'sunny' ? sol_nuvem : noite_vento}
+
+                    alt={(data.list[0].weather[0].main) === 'sunny' ? 'Sol na nuvem' : 'Lua na nuvem'} 
+
+                    className='sol_nuvem'/>
+                    {/* <img src={sol_nuvem } alt="Sol na nuvem" className='sol_nuvem'/> */}
+                </figure>
+    
+                {data && (
+                  <p>{data.list[0].weather[0].main}</p>
+                  // {data}
+                )}
+            </div>
+    
+    
+            {/* Temp.máxima */}
+            <div className='temp'>
+                <p>{Temperatura_maxima}°C</p>
+                <figure className='container_seta'>
+                    {/* imagem */}
+                    <img src={seta_cima} alt="Seta para cima" className='imagem_seta' />
+                </figure>
+            </div>
         
-    </div>
-  )
+            {/* Temperatura fixa */}
+            <p className='temp_fixa'>{Temperatura_celsius}°C</p>
+    
+            {/* Temp. mínima */}
+            <div className='temp'>
+                <p>{Temperatura_minima}°C</p>
+                <figure className='container_seta'>
+                    {/* imagem */}
+                    <img src={seta_baxio} alt="Seta para baixo" className='imagem_seta' />
+                </figure>
+            </div>       
+    
+            
+            {/* humidade */}
+            <div className='container_humidade_padrao'>
+              <figure>
+                  <img src={humidade} alt="imgame/logo de humidade" className='humidade'/>
+                  {data && (
+                      <p>{data.list[0].main.humidity}</p>
+                  )}
+                  <p className='nome_pequeno'>Humidade</p>
+              </figure>
+            </div>
+            
+            {/* pressão */}
+            <div className='container_pressao_padrao'>
+              <figure>
+                  <img src={pressao} alt="imgame/logo de aparelho de medir pressão" className='pressao'/>
+                  {data && (
+                      <p>{data.list[0].main.pressure}mBar</p>
+                  )}
+                  <p className='nome_pequeno'>Pressão</p>
+              </figure>
+            </div>
+    
+            {/* vento */}
+            <div className='container_vento_padrao'>
+                <figure>
+                    <img src={vento} alt="imagem de corrente de vento" className='vento'/>
+                    {data && (
+                        <p>{data.list[0].wind.speed}km/h</p>
+                    )}
+                    <p className='nome_pequeno'>Vento</p>
+                </figure>
+            </div>
+            
+            {/* latitude */}
+            <div className='container_vento_padrao'>
+                {data && (
+                    <p>{data.city.coord.lat}</p>
+                )}
+                <p className='nome_pequeno'>Latitude</p>
+            </div>
+    
+            {/* logintude */}
+            <div className='container_vento_padrao'>
+              {data && (
+                  <p>{data.city.coord.lon}</p>
+              )}
+              <p className='nome_pequeno'>Longitude</p>
+            </div>
+        
+            {/* sensação térmica */}
+            <div className='container_vento_padrao'>
+              {Sensacao_termica}°C
+              <p className='nome_pequeno'>Sensação Térmica</p>
+            </div>
+          
+            
+        </div>
+      )
+  }
 }
 
 export default BoxData
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,14 @@ import App from './App.jsx'
 import './index.css'
 import { QueryClient, QueryClientProvider } from 'react-query'
 
+// É obrigatório fazer isso
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   
   <React.StrictMode>
-      <App />
+    <QueryClientProvider client={queryClient}>
+        <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
